495c69c32a68a73a7cafd29104a92d56e3f94f13
Joaquim Neto <joaquimmy@gmail.com>
2023-06-12 22:06:30 -0300

    Initial Commit

---
+++ src/main/java/com/codekaffe/kafsmobs/KafsMobsClient.java
@@ -0,0 +1,11 @@
+package com.codekaffe.kafsmobs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KafsMobsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
